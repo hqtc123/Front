02c1e989fd9cce6e6124d31d5108ffe8c452582f
heqing@superid.cn <heqing@superid.cn>
2014-09-12 16:57:48 +0800

    init

---
+++ app/js/controllers.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/* Controllers */
+
+var jinJuControllers = angular.module('jinJuControllers', []);
+
+jinJuControllers.controller('PhoneListCtrl', ['$scope', '$http',
+  function($scope, $http) {
+    $http.get('phones/phones.json').success(function(data) {
+      $scope.phones = data;
+    });
+
+    $scope.orderProp = 'age';
+  }]);
+
+phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams',
+  function($scope, $routeParams) {
+    $scope.phoneId = $routeParams.phoneId;
+  }]);
